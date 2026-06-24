c5c343fd8788f86e4f71142b54873e1dd35be9ea
Briiia69 <briiia@proton.me>
2026-06-24 18:23:34 +0200

    ew

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,18 @@
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	i;
+	int	signy
+
+	res = 0;
+	i = 0;
+	while (str[i] && (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			res = -
+
+
+
+		if (str[i] < '0 || str[i] > '9')
+			return (
+
