fb02b841b0ccf49ab0148d8add74a23cb3510d3d
Saliinger <alexisnoukan@gmail.com>
2026-06-24 23:50:29 +0200

    hello

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -1,18 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anoukan <alnoukan@student.42lausanne.ch>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 23:29:14 by anoukan           #+#    #+#             */
+/*   Updated: 2026/06/24 23:49:05 by anoukan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
 int	ft_atoi(char *str)
 {
-	int	res;
-	int	i;
-	int	signy
+	long long	res;
+	int			sign;
+	int			i;
 
 	res = 0;
+	sign = 1;
 	i = 0;
-	while (str[i] && (str[i] == '-' || str[i] == '+')
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
 	{
 		if (str[i] == '-')
-			res = -
+			sign = -1;
+		i++;
+	}
+	if (str[i] && (str[i] == '+' || str[i] == '-'))
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return ((int)(res * sign));
+}
 
-
-
-		if (str[i] < '0 || str[i] > '9')
-			return (
-
+int	main(int ac, char *av[])
+{
+	if (ac < 1)
+		return (1);
+	printf("%d\n", ft_atoi(av[1]));
+	return (0);
+}
